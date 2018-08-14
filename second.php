4ac0adc1e1b521dbb0fab7dbb9c12e9a8f6a75d6
Jiashu Wang <jwang-intern@bbmtek.com>
2018-08-14 15:24:30 -0400

    pq

---
+++ second.php
@@ -0,0 +1,16 @@
+<?php
+   echo "<h2>Your Given details are as :</h2>";
+   echo $name;
+   echo "<br>";
+
+   echo $email;
+   echo "<br>";
+
+   echo $website;
+   echo "<br>";
+
+   echo $comment;
+   echo "<br>";
+
+   echo $gender;
+?>
